f5efaa12deae030d568d02df10cd8ecf56e61508
Simone van Zeijl <simonev.zeijl@hotmail.nl>
2020-03-30 19:51:02 +0200

    update

--- static/js/index.js
+++ static/js/index.js
@@ -1,120 +1,44 @@
 const shareButton = document.querySelector("[share-btn]");
-// const listsContainer = document.querySelector("[data-lists]");
-// const newListForm = document.querySelector("[data-new-list-form]");
-// const newListInput = document.querySelector("[data-new-list-input]");
-// const deleteButton = document.querySelector("[delete-btn]");
-// // const shareButton = document.querySelector("[share-btn]");
-// // Save and store things on the user's browsers
-// // Create keys to store information
-// // Make a name space, it prevents you from overwriting information that is already in the local storage
-// const LOCAL_STORAGE_LIST_KEY = "task.lists";
+const resetButton = document.querySelector(".reset");
 
-// // the list is getting stored in task.lists
-// // get the information from the localstorage with json.parse
-// // it's getting out in a object or give us a empty array to start (default)
-// let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
+// Local storage save
 
-// newListForm.addEventListener("submit", element => {
-//   // every time when you click on enter, the pages refreshed, preventDefault prevents this
-//   element.preventDefault();
-//   const listName = newListInput.value;
-//   // user have to type something
-//   if (listName == null || listName === " ") return;
-//   const list = createList(listName);
-//   newListInput.value = null;
-//   lists.push(list);
-//   saveAndRender();
-// });
+const allInputs = document.querySelectorAll("input");
 
-// function createList(name) {
-//   return {
-//     id: Date.now().toString(),
-//     name: name
-//   };
-// }
+allInputs.forEach(x => {
+  console.log(x);
+  console.log(x.id);
 
-// function render() {
-//   clearElement(listsContainer);
-//   renderLists();
-// }
+  if (localStorage.getItem(x.id)) {
+    console.log(`id ${x.id} zit erin!`);
+    x.value = JSON.parse(localStorage.getItem(x.id));
+  }
 
-// function renderLists() {
-//   lists.forEach(list => {
-//     const listElement = document.createElement("li");
-//     listElement.classList.add("list-name");
-//     listElement.innerText = list.name;
-//     listsContainer.appendChild(listElement);
-//   });
-// }
+  x.addEventListener("keydown", onEnter);
+});
 
-// function saveAndRender() {
-//   save();
-//   render();
-// }
+function onEnter(e) {
+  if (e.keyCode === 13) {
+    const inputId = e.target.id;
+    const inputValue = e.target.value;
+    console.log({ inputId, inputValue });
 
-// function save() {
-//   localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
+    localStorage.setItem(inputId, JSON.stringify(inputValue));
+    console.log(localStorage.getItem(inputId));
+  }
+}
 
-//   //if localStorage.getItem(/dewaardediejewilthebben/)
-//   //input.innerHTML = localStorage.getItem()
-//   //localStorage.removeItem(/waarde die wegmoet/)
-// }
+console.log(allInputs);
 
-// function clearElement(element) {
-//   while (element.firstChild) {
-//     element.removeChild(element.firstChild);
-//   }
-// }
+// Local storage resetten
+resetButton.addEventListener("click", resetLocalStorage);
 
-// render();
-// saveAndRender();
+function resetLocalStorage() {
+  localStorage.clear();
+  inputValue = " ";
+}
 
-// function deleteList() {
-//   deleteButton.addEventListener("click", function() {
-//     if (confirm("Are you sure you want to delete this list?")) {
-//       // Delete it!
-//       clearElement(listsContainer);
-//       localStorage.clear();
-//       location.reload();
-//     } else {
-//       // Do nothing!
-//     }
-//   });
-// }
-// deleteList();
-
-// Local storage save values
-// BRON: https://www.youtube.com/watch?v=k8yJCeuP6I8
-// const name = document.getElementById("input-1");
-// const age = document.getElementById("input-2");
-// // const code = document.getElementById("input-3");
-// // const teacher = document.getElementById("input-4");
-// // const course = document.getElementById("input-5");
-// const button = document.getElementById("LocalStorage");
-// const localStorageOutput = document.getElementById("LocalStorageOutput");
-
-// button.onclick = function() {
-//   const name = name.value;
-//   const age = age.value;
-//   // const code = code.value;
-//   // const teacher = teacher.value;
-//   // const course = course.value;
-
-//   console.log(name);
-//   console.log(age);
-
-//   localStorage.setItem(name, age, code, teacher, course);
-//   location.reload();
-// };
-
-// for (let i = 0; i < localStorage.length; i++) {
-//   const name = localStorage.name(i);
-//   const age = localStorage.getItem(name);
-
-//   localStorageOutput.innerHTML += `${name}: ${age}`;
-// }
-
-// // Titel aanpassen aan input
+// Titel aanpassen aan input
 
 let title2 = document.getElementById("title");
 let firstname = document.getElementById("firstname");
@@ -155,3 +79,98 @@ if (overlay) {
     shareModal.classList.remove("show-share");
   });
 }
+
+// Copy Button Save link
+
+// (function() {
+//   var copyButton = document.querySelector(".copy");
+//   var copyInput = document.querySelector(".save-page article a");
+//   copyButton.addEventListener("click", function(e) {
+//     e.preventDefault();
+//     var text = copyInput.select();
+//     document.execCommand("copy");
+//   });
+
+//   copyInput.addEventListener("click", function() {
+//     this.select();
+//   });
+// })();
+
+// (function() {
+//   var copylinkbtn = document.getElementById("copy-link-btn"),
+//     copylink = document.getElementById("copy-link-wrapper"),
+//     overlay = document.getElementById("overlay");
+
+//   copylinkbtn.addEventListener(
+//     "click",
+//     function() {
+//       var error = document.getElementsByClassName("error");
+
+//       while (error[0]) {
+//         error[0].parentNode.removeChild(error[0]);
+//       }
+
+//       document.body.className += " active";
+
+//       copylink.children[2].value = window.location.href;
+//       copylink.children[2].focus();
+//       copylink.children[2].select();
+//     },
+//     false
+//   );
+
+//   overlay.addEventListener(
+//     "click",
+//     function() {
+//       document.body.className = "";
+//     },
+//     false
+//   );
+
+//   copylink.children[2].addEventListener(
+//     "keydown",
+//     function(e) {
+//       var error = document.getElementsByClassName("error");
+
+//       while (error[0]) {
+//         error[0].parentNode.removeChild(error[0]);
+//       }
+
+//       setTimeout(function() {
+//         if (
+//           (e.metaKey || e.ctrlKey) &&
+//           e.keyCode === 67 &&
+//           isTextSelected(copylink.children[2])
+//         ) {
+//           document.body.className = "";
+//         } else if (
+//           (e.metaKey || e.ctrlKey) &&
+//           e.keyCode === 67 &&
+//           isTextSelected(copylink.children[2]) === false
+//         ) {
+//           var error = document.createElement("span");
+//           error.className = "error";
+//           var errortext = document.createTextNode(
+//             "The link was not copied, make sure the entire text is selected."
+//           );
+
+//           error.appendChild(errortext);
+//           copylink.appendChild(error);
+//         }
+//       }, 100);
+
+//       function isTextSelected(input) {
+//         if (typeof input.selectionStart == "number") {
+//           return (
+//             input.selectionStart == 0 &&
+//             input.selectionEnd == input.value.length
+//           );
+//         } else if (typeof document.selection != "undefined") {
+//           input.focus();
+//           return document.selection.createRange().text == input.value;
+//         }
+//       }
+//     },
+//     false
+//   );
+// })();
